213ecc75d6d2a28b76bfb5c109985fae5f041717
Dean Lockgaard <dean.lockgaard@gmail.com>
2026-06-05 14:24:16 -0700

    Add GitHub Actions deployment pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://arbitercontrol.com',
   vite: {
     plugins: [tailwindcss()]
   }
